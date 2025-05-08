c4c6d9a18ad332dcd0b04f993dc5b5cfef17d95c
Mikkres <miguelfl2603@gmail.com>
2025-05-07 21:33:48 -0500

    modificacion 7/5

--- src/app/products/list/list.component.ts
+++ src/app/products/list/list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CardProductComponent } from '../../shared/components/card-product/card-product.component';
+import { PaginatorComponent } from '../../shared/components/paginator/paginator.component';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [CardProductComponent, PaginatorComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
+
+
 })
 export class ListComponent {
 
